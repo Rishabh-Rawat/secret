1dc21ce7dd3dec0cc06ee6d437c30ce027782603
Rishabh Rawat <rishabhrawat.rishu@gmail.com>
2023-01-29 10:51:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -126,9 +126,9 @@ app.get("/submit", function (req, res) {
   }
 });
 
-app.post("/submit", async function (req, res) {
+app.post("/submit", function (req, res) {
   const submittedSecret = req.body.secret;
-  await User.findOneAndUpdate(
+  User.findOneAndUpdate(
     { _id: req.user._id },
     { secret: submittedSecret },
     function (err, foundUser) {
