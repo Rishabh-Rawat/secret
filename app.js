420ea78bf4d529888a1b9204c9c98493da54f9e2
Rishabh-Rawat <rishabhrawat.rishu@gmail.com>
2023-02-04 13:03:41 +0530

    updated enviromnent variable

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ passport.use(
       clientID: process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SECRET,
       // callbackURL: "http://localhost:3000/auth/google/secrets",
-      callbackURL: process.env.CYCLIC_URL + "/auth/google/secrets",
+      callbackURL: process.env.URL + "/auth/google/secrets",
     },
     function (accessToken, refreshToken, profile, cb) {
       // console.log(profile);
